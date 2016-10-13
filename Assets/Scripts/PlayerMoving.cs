8fecf3f9d477e4783e5ff888004a1897bd0a304a
Zergling <death1681@yandex.ru>
2016-10-13 21:01:45 +0300

    first character moving realization

---
+++ Assets/Scripts/PlayerMoving.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoving : MonoBehaviour
+{
+    public GameObject sphere;
+    private Transform sphereTransform;
+    private Transform myTransform;
+    public float speed;
+    public Vector3 movementVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sphereTransform = sphere.transform;
+        myTransform = this.transform;
+        movementVector = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        movementVector.y = Input.GetAxis("Vertical");
+        movementVector.x = Input.GetAxis("Horizontal");
+
+        myTransform.position = Vector3.Lerp(myTransform.position, myTransform.position + sphereTransform.forward * movementVector.y * speed, Time.deltaTime * 5);
+        myTransform.position = Vector3.Lerp(myTransform.position, myTransform.position + sphereTransform.right * movementVector.x * speed, Time.deltaTime * 5);
+    }
+}
