15ed3e012492e4798defd90f2085cb8f584d2a03
Zergling <death1681@yandex.ru>
2016-10-15 00:18:12 +0300

    door completed. level reconstructed

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour
+{
+    bool opened;
+    Animation ani;
+    public GameObject buttonInteract;
+
+    // Use this for initialization
+    void Start ()
+    {
+        opened = false;
+        ani = this.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    public void interact()
+    {
+        if (opened == false)
+        {
+            ani.CrossFade("doorOpen");
+            opened = true;
+        }
+        else
+        {
+            ani.CrossFade("doorClose");
+            opened = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag.Equals("Player"))
+        {
+            buttonInteract.active = true;
+            buttonInteract.SendMessage("setDoor", this.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        buttonInteract.active = false;
+    }
+}
---
+++ Assets/Scripts/buttonInteractScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonInteractScript : MonoBehaviour
+{
+    private GameObject nearDoor;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void setDoor(GameObject door)
+    {
+        nearDoor = door;
+    }
+
+    public void onClick()
+    {
+        if (nearDoor != null)
+        {
+            nearDoor.SendMessage("interact");
+        }
+    }
+}
