0dd8c4a96651e6f346fe116f913e6ee415221077
Zergling <death1681@yandex.ru>
2016-10-14 20:55:54 +0300

    terminal completed

---
+++ Assets/Scripts/LockPanelScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LockPanelScript : MonoBehaviour
+{
+    #region Unity scene setting
+    [SerializeField]
+    private Text[] TextFields;
+    #endregion
+
+    private string codeString;
+    private bool isTrue;
+
+    int[] code;
+
+    // Use this for initialization
+    void Start ()
+    {
+        code = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            code[i] = 0;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Inc(int index)
+    {
+        code[index]++;
+        if (code[index] > 9)
+        {
+            code[index] = 0;
+        }
+        TextFields[index].text = code[index].ToString();
+    }
+
+    public void Dec(int index)
+    {
+        code[index]--;
+        if (code[index] < 0)
+        {
+            code[index] = 9;
+        }
+        TextFields[index].text = code[index].ToString();
+    }
+
+    public void SetCodeString(string tCodeString)
+    {
+        codeString = tCodeString;
+    }
+
+    public void SetTrue(bool tIsTrue)
+    {
+        isTrue = tIsTrue;
+    }
+
+    public void Confirm()
+    {
+        string thisCode = "";
+        for (int i = 0; i < TextFields.Length; i++)
+        {
+            thisCode += TextFields[i].text;
+        }
+
+        GameObject.Destroy(gameObject);
+
+        if (isTrue == true)
+        {
+            Debug.Log("TRUE TERMINAL");
+            // make a signal
+            if (thisCode.ToLower().Equals(codeString.ToLower()))
+            {
+                Debug.Log("SIGNAL TO USB PORT");
+                return;
+            }
+        }
+
+        // else - nothing to do here;
+        Debug.Log("NO SIGNAL");
+    }
+}
---
+++ Assets/Scripts/Terminal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terminal : MonoBehaviour
+{
+    public bool isTrueTerminal;
+    public string code;
+    public GameObject panel;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void onClick()
+    {
+        GameObject obj = Instantiate<GameObject>(panel);
+        obj.SendMessage("SetCodeString", code);
+        obj.SendMessage("SetTrue", isTrueTerminal);
+    }
+}
