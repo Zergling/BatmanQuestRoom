67a527bfb09fdc78b69aa51815f29a47ee2ce421
Zergling <death1681@yandex.ru>
2016-10-20 14:31:56 +0300

    flashlight completed

---
+++ Assets/Scripts/buttonFlashlight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonFlashlight : MonoBehaviour
+{
+    public GameObject light;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void onClick()
+    {
+        light.SetActive(!light.active);
+    }
+}
