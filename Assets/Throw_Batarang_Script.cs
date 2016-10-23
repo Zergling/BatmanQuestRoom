b7f9e05b64a574a150a988e091ce3fbded5344a2
Nikita <mexanik.o@gmail.com>
2016-10-24 00:03:34 +0600

    added batarang throw animation and button for thow

---
+++ Assets/Throw_Batarang_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw_Batarang_Script : MonoBehaviour 
+{
+
+	GameObject character;
+	Animator char_anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		character = GameObject.Find("ThirdPersonController");
+		char_anim = character.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void onClick()
+	{
+		char_anim.SetTrigger("Throw_Batarang");
+	}
+}
