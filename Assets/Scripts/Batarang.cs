abddc2b70f0fefd8cdd7704408af09d12ae00e72
Zergling <death1681@yandex.ru>
2016-10-28 17:51:29 +0300

    batarangs part 2

--- Assets/Scripts/Batarang.cs
+++ Assets/Scripts/Batarang.cs
@@ -1,31 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Batarang : MonoBehaviour
 {
     public static string BATARANG_TAG = "_Batarang";
+    public static Queue<GameObject> batarangs = new Queue<GameObject>();
     public float speed;
     Camera camera;
     Vector3 direction;
     Transform myTransform;
+    bool isMoving;
 	// Use this for initialization
 	void Start ()
     {
         camera = Camera.main;
         direction = camera.transform.forward;
         myTransform = transform;
+        isMoving = true;
+
+        if (batarangs.Count >= 10)
+        {
+            Debug.Log("max batarangs!");
+            GameObject first = batarangs.Dequeue();
+            Destroy(first);
+            batarangs.Enqueue(gameObject);
+        }
+        else
+        {
+            batarangs.Enqueue(gameObject);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        myTransform.position = Vector3.Lerp(myTransform.position, myTransform.position + direction * speed, Time.deltaTime);
-        myTransform.Rotate(0, 20, 0);
+        if (isMoving == true)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, myTransform.position + direction * speed, Time.deltaTime);
+            myTransform.Rotate(0, 20, 0);
+        }
 	}
 
     void OnCollisionEnter (Collision c)
     {
         Debug.Log("destryo");
-        Destroy(gameObject);
+        isMoving = false;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 }
