276f5fc71ed4b7fe477721013284320ce99ab50a
Zergling <death1681@yandex.ru>
2016-10-28 15:32:31 +0300

    batarang added

---
+++ Assets/Scripts/Batarang.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Batarang : MonoBehaviour
+{
+    public float speed;
+    Camera camera;
+    Vector3 direction;
+    Transform myTransform;
+	// Use this for initialization
+	void Start ()
+    {
+        camera = Camera.main;
+        direction = camera.transform.forward;
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTransform.position = Vector3.Lerp(myTransform.position, myTransform.position + direction * speed, Time.deltaTime);
+        myTransform.Rotate(0, 20, 0);
+	}
+
+    void OnCollisionEnter (Collision c)
+    {
+        Debug.Log("destryo");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BatmanBatarangThrowingScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatmanBatarangThrowingScript : MonoBehaviour
+{
+    public GameObject SpawnPoint;
+    public GameObject Batarang;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void Throw()
+    {
+        Debug.Log("throwing!!");
+        Instantiate(Batarang, SpawnPoint.transform.position, Quaternion.identity);
+    }
+}
