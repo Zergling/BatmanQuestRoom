dbfeff7ef5a826a299319406bb4fc8dea95a1b91
Zergling <death1681@yandex.ru>
2016-11-03 18:05:46 +0300

    doors now open with batarangs

---
+++ Assets/Scripts/DoorBatarangOpenScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorBatarangOpenScript : MonoBehaviour
+{
+    private GameObject door;
+
+	// Use this for initialization
+	void Start ()
+    {
+        door = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.collider.tag.Equals(Batarang.BATARANG_TAG))
+        {
+            door.SendMessage("interact");
+        }
+    }
+}
