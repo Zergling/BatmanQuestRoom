16483ec2c7ca45b96d96de37d9a03f7d38cf0180
Zergling <death1681@yandex.ru>
2016-11-03 18:55:47 +0300

    new terminal incoming

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -54,7 +54,7 @@ public class DoorScript : MonoBehaviour
             Vector3 batpos = obj.transform.position;
             float d = Mathf.Sqrt(Mathf.Pow(mypos.x - batpos.x, 2) + Mathf.Pow(mypos.y - batpos.y, 2) + Mathf.Pow(mypos.z - batpos.z, 2));
 
-            if (d <= 2.0f)
+            if (d <= 3.0f)
             {
                 Destroy(obj);
             }
