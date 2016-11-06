36fb9757517af573bc55e17a3be11820058d5976
Nikita <mexanik.o@gmail.com>
2016-11-07 02:37:59 +0600

    police cars add

---
+++ Assets/Scripts/PoliceLights.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliceLights : MonoBehaviour 
+{
+
+	[SerializeField] Light redLight;
+	[SerializeField] Light blueLight;
+
+	private Vector3 redTemp;
+	private Vector3 blueTemp;
+
+	[SerializeField] int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		redTemp.y += speed * Time.deltaTime;
+		blueTemp.y -= speed * Time.deltaTime;
+
+		redLight.transform.eulerAngles = redTemp;
+		blueLight.transform.eulerAngles = blueTemp;
+	}
+}
