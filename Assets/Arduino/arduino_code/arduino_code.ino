40e74de46f316d78559369c65a66ba787c3b3d30
Nikita <mexanik.o@gmail.com>
2016-11-11 20:44:06 +0600

    arduino semi funal

--- Assets/Arduino/arduino_code/arduino_code.ino
+++ Assets/Arduino/arduino_code/arduino_code.ino
@@ -7,15 +7,28 @@ void setup()
   Serial.begin(9600);
   while (!Serial);
 
+  analogWrite(A4, 0);
+  
+  //digitalWrite(A4, HIGH);
+  //digitalWrite(A4, LOW);
+
   //sCmd.addCommand("PING", pingHandler);
 }
 
 void loop () 
 {
+    /*analogWrite(A4, 255);
+    delay(5000);
+    analogWrite(A4, 0);
+    delay(5000);*/
+  
   if (Serial.available() > 0)
   {
     sCmd.readSerial();
     Serial.println("PONG");
+    analogWrite(A4, 255);
+    delay(5000);
+    analogWrite(A4, 0);
   }
 }
 
