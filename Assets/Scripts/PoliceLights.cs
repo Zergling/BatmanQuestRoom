2c6afc1bf6cd9dc65340766b42f5d341686467f3
Nikita <mexanik.o@gmail.com>
2016-11-11 21:34:31 +0600

    police lights potugi

--- Assets/Scripts/PoliceLights.cs
+++ Assets/Scripts/PoliceLights.cs
@@ -11,6 +11,7 @@ public class PoliceLights : MonoBehaviour
 	private Vector3 blueTemp;
 
 	[SerializeField] int speed;
+	[SerializeField] Vector3 gap;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +24,7 @@ public class PoliceLights : MonoBehaviour
 		redTemp.y += speed * Time.deltaTime;
 		blueTemp.y -= speed * Time.deltaTime ;
 
-		redLight.transform.eulerAngles = redTemp;
-		blueLight.transform.eulerAngles = blueTemp;
+		redLight.transform.eulerAngles = redTemp + gap;
+		blueLight.transform.eulerAngles = blueTemp + gap;
 	}
 }
